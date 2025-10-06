565f1bf91f1c752ba22573a47653b4b4ef944406
JaYRaNa213 <tresurejay6@gmail.com>
2025-10-06 20:48:18 +0530

    cyclic sort

---
+++ Array/CyclicSort.java
@@ -0,0 +1,86 @@
+/// 1. you have given 0 to n number find missing number and time (n)  and space (1)
+
+
+// public class CyclicSort {
+
+//   public static void main(String []args){
+
+//     int [] arr = {1,5,3,4,6,8,7,9};
+//     int i =0;
+
+//     while (i < arr.length){
+
+//       int correct = arr[i] - 1;
+
+//       if (arr[i] != arr[correct]){
+
+//         swap(arr[i] , arr[correct],arr);
+//       }
+
+//     }
+
+//     System.out.println(arr);
+
+    
+
+
+//   }
+
+//   static void swap(int i , int j, int []arr){
+
+//     int temp =  arr[i];
+//     arr[i] = arr[j];
+
+//     arr[j] = temp;
+
+
+//   }
+
+
+
+
+// }
+
+
+
+/// 2 find the missing number from the given array , while given array is from 0 to n;
+/// 
+//
+public class CyclicSort {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 1, 5, 3, 7, 9, 10, 6};
+
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+
+            //  Check: element must be in range [1, arr.length]
+            if ( arr[i] != arr[correct]) {
+                swap(i, correct, arr);
+            } else {
+                i++;
+            }
+        }
+
+        //  Print sorted array
+        System.out.print("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        // Identify missing numbers safely
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] != i + 1) {
+                System.out.println("Missing or misplaced number: " + (i + 1));
+            }
+        }
+    }
+
+    public static void swap(int i, int j, int[] arr) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
